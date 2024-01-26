9bf7dd2e1f061e6e7f55c917ccd22d52934e3e32
Fahmi Fauzi <fahmi.fauzi@ifg-life.id>
2024-01-26 19:52:51 +0700

    styling input, add react toast hot

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,51 @@
 "use client"
 
+import { useState } from "react"
+import toast, { Toaster } from 'react-hot-toast';
+
+// component
 import PlaneIcon from "@/public/svg/PlaneIcon"
 import Spinner from "@/public/svg/Spinner"
-import { useState } from "react"
 
 const Home = () => {
 
+  
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<any>('');
 
+  const handleChangeInput = (e: any) => {
+    const value = e.target.value
+    setInputValue(value)
+  }
+  
   const handleSendAction = async () => {
     setIsLoading(true)
     await setTimeout(() => {
       setIsLoading(false)
+      toast('Message sent successfully!');
+      setInputValue('')
     }, 2000);
   }
 
   return (
     <section className="w-full h-dvh bg-instagram">
+      <Toaster />
       {/* content */}
-      <div className="w-full h-full flex justify-center items-center p-[14px]">
-        
-        <form className="flex flex-col">
-          <label htmlFor="">Write Message as Annonim</label>
-          <textarea name="" id="" cols={100} ></textarea>
+      <div className="w-full h-full flex justify-center items-center p-4">
+        <form className="flex w-full sm:w-fit flex-col">
+          <label htmlFor=""></label>
+          <input type="text" placeholder="Write a message here secretly..." value={inputValue} onChange={(e) => handleChangeInput(e)} />
 
-          <div className="w-full flex justify-end mt-7">
+          <div className="w-full flex justify-end mt-5">
             <button type="button" className="inline-flex h-12 items-center justify-center rounded-md bg-oceanic pl-5 pr-6 font-medium transition active:scale-110 gap-2" onClick={handleSendAction}>
               {!isLoading ? (
-                <>
-                <PlaneIcon />
-                Send Message
-                </>
+                <><PlaneIcon />Send Message</>
               ) : (
-                <>
-                <div className="animate-spin">
-                  <Spinner />
-                </div>
-                Sending Message...
-                </>
+                <><div className="animate-spin"><Spinner /></div><span>Sending Message...</span></>
               )}
               </button>
           </div>
+                
 
         </form>
       </div>
